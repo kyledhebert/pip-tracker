747551cb64f028b3da0fceec02b5da469bb36670
Kyle Hebert <kyle.hebert@gmail.com>
2017-12-03 10:58:31 -0600

    Add Boilerplate for Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DateComponent } from './components/date/date.component';
+import { ProgressComponent } from './components/progress/progress.component';
+import { ExpensesComponent } from './components/expenses/expenses.component';
+import { ExpenseComponent } from './components/expense/expense.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DateComponent,
+    ProgressComponent,
+    ExpensesComponent,
+    ExpenseComponent
   ],
   imports: [
     BrowserModule
