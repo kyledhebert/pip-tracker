9fd0a40ab9b75f4aae85493fa8afef01e0ab6d54
kylehebert <kyle.hebert@gmail.com>
2018-01-01 16:29:30 -0600

    Complete Progress Component

--- src/app/components/progress/progress.component.ts
+++ src/app/components/progress/progress.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, Input } from '@angular/core';
+import * as _ from 'lodash';
+
+import { Expense } from './../../models/expense';
+import { PipService } from '../../pip.service';
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
   styleUrls: ['./progress.component.css']
 })
-export class ProgressComponent implements OnInit {
+export class ProgressComponent implements OnChanges {
+
+  @Input()
+  expenses: Expense[];
+
+  @Input()
+  goal: number;
+
+  @Input()
+  totalSpent: number;
+
+  progress: number;
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges() {
+    this.progress = (this.totalSpent / this.goal) * 100;
   }
 
 }
