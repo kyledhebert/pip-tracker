7a83fa08c6e79b1f4a3fee1cd3797c01894b3e82
21STSERVICES\khebert <khebert@itm21st.com>
2018-01-07 12:27:09 -0600

    Add Files and Scripts for Creating Windows Installer

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-const {app, BrowserWindow} = require('electron')
+ //handle setupevents as quickly as possible
+ const setupEvents = require('./installers/setupEvents')
+ if (setupEvents.handleSquirrelEvent()) {
+    // squirrel event handled and app will exit in 1000ms, so don't do anything else
+    return;
+ }
+const {app, BrowserWindow} = require('electron');
 const path = require('path')
 const url = require('url')
 
